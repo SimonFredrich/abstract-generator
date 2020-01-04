96ce948cc41bce2022f86d6e4ea9dae7d904cfb9
SimonFredrich <21simplex02@gmail.com>
2020-01-04 01:22:27 +0100

    implement sentence separation

--- process.py
+++ process.py
@@ -2,6 +2,27 @@
 this module processed the text and produces an abstract
 """
 
-
 def processText(text):
-    return text.upper()
+    # split the text into words
+    words = text.split()
+
+    # filter words with dot
+    words_with_dot = [0]
+    for word in words:
+        for char in word:
+            if char == ".":
+                words_with_dot.append(words.index(word))
+
+    # seperate all sentences into an array
+    sentences = []
+    for index_i, item in enumerate(words_with_dot):
+        if index_i != 0:
+            if words_with_dot[index_i - 1] != 0:
+                sentences.append(words[words_with_dot[index_i-1]+1:item + 1])    
+            else:
+                sentences.append(words[words_with_dot[index_i - 1]:item + 1])
+
+    # next join the sentences together
+    print(sentences)
+
+    return text
